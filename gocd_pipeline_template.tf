98f5494852d82f2ae31348093a1a1d3353058cb0
Keshav-Rai001 <42931112+Keshav-Rai001@users.noreply.github.com>
2018-09-21 14:29:59 +0530

    Add files via upload

---
+++ gocd_pipeline_template.tf
@@ -0,0 +1,10 @@
+
+
+provider "gocd"
+{
+	baseurl="${var.baseurl}"
+	#host = "https://192.168.99.100:8443" 
+}
+resource "gocd_pipeline_template" "terraform-builder" {
+  name = "${var.template_name}"
+}
